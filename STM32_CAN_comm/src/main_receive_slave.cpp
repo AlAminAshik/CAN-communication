fcde6d5282a7dca3da7c686301b321a022c83daf
AlAminAshik <alaminashik890.bd@gmail.com>
2025-07-28 00:52:50 +0600

    .

--- STM32_CAN_comm/src/main_receive_slave.cpp
+++ STM32_CAN_comm/src/main_receive_slave.cpp
@@ -1,75 +1,75 @@
-// // CAN Send Example
+// CAN Send Example
 
-// // connection
-// // stm32f103 bluepill-----------MCP2515
-// // 5V --------------- 5V
-// // GND -------------- GND
-// // A5 ------------- SCK
-// // A6 ------------- MISO
-// // A7 ------------- MOSI
-// // A4 ------------- CS (Chip Select)
+// connection
+// stm32f103 bluepill-----------MCP2515
+// 5V --------------- 5V
+// GND -------------- GND
+// A5 ------------- SCK
+// A6 ------------- MISO
+// A7 ------------- MOSI
+// A4 ------------- CS (Chip Select)
 
-// #include <mcp_can.h>
-// #include <SPI.h>
+#include <mcp_can.h>
+#include <SPI.h>
 
-// uint16_t myAddress = 0x100;
+uint16_t myAddress = 0x100;
 
-// long unsigned int rxId;
-// unsigned char len = 0;
-// unsigned char rxBuf[7];
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[7];
 
-// MCP_CAN CAN0(PA4);  // Set CS to pin PA4
-// #define CAN0_INT PA8 // Set INT to pin PA8
+MCP_CAN CAN0(PA4);  // Set CS to pin PA4
+#define CAN0_INT PA8 // Set INT to pin PA8
 
-// const char myString[] = "Hi"; // 2 bytes of data
+const char myString[] = "Hi"; // 2 bytes of data
 
-// // run when interrupt is triggered
-// void canInterrupt()
-// {
-//   Serial.println("CAN Interrupt Triggered!");
-//   if (!digitalRead(CAN0_INT)) // If CAN0_INT pin is low, read receive buffer
-//   {
-//     Serial.print("Master is asking data ");
-//     CAN0.readMsgBuf(&rxId, &len, rxBuf); // Read data: len = data length, buf = data byte(s)
-//     if (rxId == myAddress)
-//     {
-//       Serial.println("from Me!!");
+// run when interrupt is triggered
+void canInterrupt()
+{
+  Serial.println("CAN Interrupt Triggered!");
+  if (!digitalRead(CAN0_INT)) // If CAN0_INT pin is low, read receive buffer
+  {
+    Serial.print("Master is asking data ");
+    CAN0.readMsgBuf(&rxId, &len, rxBuf); // Read data: len = data length, buf = data byte(s)
+    if (rxId == myAddress)
+    {
+      Serial.println("from Me!!");
 
-//       // send only button state (0 or 1)///
-//         byte sndStat1 = CAN0.sendMsgBuf(myAddress, 0, 2, (byte*)&myString); //address, data length, data pointer
-//         Serial.println("I sent data with my address 0x100");
-//         if (sndStat1 == CAN_OK) {
-//            Serial.println("Message Sent properly!");
-//            } else {
-//            Serial.println("Error Sending Frames...");
-//            }
-//        }
+      // send only button state (0 or 1)///
+        byte sndStat1 = CAN0.sendMsgBuf(myAddress, 0, 2, (byte*)&myString); //address, data length, data pointer
+        Serial.println("I sent data with my address 0x100");
+        if (sndStat1 == CAN_OK) {
+           Serial.println("Message Sent properly!");
+           } else {
+           Serial.println("Error Sending Frames...");
+           }
+       }
 
-//     else
-//       Serial.println("from someone else!");
-//   }
-// }
+    else
+      Serial.println("from someone else!");
+  }
+}
 
-// void setup()
-// {
-//   Serial.begin(115200);
+void setup()
+{
+  Serial.begin(115200);
 
-//   // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
-//   if (CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK)
-//     Serial.println("MCP2515 Initialized Successfully!");
-//   else
-//     Serial.println("Error Initializing MCP2515...");
+  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  if (CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK)
+    Serial.println("MCP2515 Initialized Successfully!");
+  else
+    Serial.println("Error Initializing MCP2515...");
 
-//   CAN0.setMode(MCP_NORMAL);                                                // Change to normal mode to allow messages to be transmitted
-//   attachInterrupt(digitalPinToInterrupt(CAN0_INT), canInterrupt, FALLING); // Attach interrupt to CAN0_INT pin
-//   // pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
-// }
+  CAN0.setMode(MCP_NORMAL);                                                // Change to normal mode to allow messages to be transmitted
+  attachInterrupt(digitalPinToInterrupt(CAN0_INT), canInterrupt, FALLING); // Attach interrupt to CAN0_INT pin
+  // pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+}
 
-// void loop()
-// {
+void loop()
+{
 
-// }
+}
 
-// /*********************************************************************************************************
-//   END FILE
-// *********************************************************************************************************/
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
